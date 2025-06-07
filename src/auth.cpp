0e3ae33566ed8d7352fe84602cfc40c418361c1b
mintRaven <debjeetbanerjee48@gmail.com>
2025-06-07 11:56:55 +0530

    [CHORE]: add password hash and salt handler functions

--- src/auth.cpp
+++ src/auth.cpp
@@ -43,4 +43,62 @@ void generateSalt(uint8_t *salt) {
       salt[i] ^= (chipId >> (i * 8)) & 0xFF;
       salt[i + 4] ^= (time >> (i * 8)) & 0xFF;
     }
+}
+
+bool passwordHashExists() {
+  EEPROM.begin(EEPROM_SIZE);
+  uint32_t magic;
+  EEPROM.get(MAGIC_ADDRESS, magic);
+  EEPROM.end();
+  return (magic == MAGIC_BYTES);
+}
+
+void storePasswordHash(String password) {
+  uint8_t hash[32];
+  uint8_t salt[16];
+  uint32_t magic = MAGIC_BYTES;
+
+  generateSalt(salt);
+  _generate_hash(password, hash, salt);
+
+  EEPROM.begin(EEPROM_SIZE);
+  for (int i = 0; i < 32; i++) {
+    EEPROM.write(HASH_ADDRESS + i, hash[i]);
   }
+  for (int i = 0; i < 16; i++) {
+    EEPROM.write(SALT_ADDRESS + i, salt[i]);
+  }
+
+  EEPROM.put(MAGIC_ADDRESS, magic);
+  EEPROM.commit();
+  EEPROM.end();
+
+  Serial.println("\033[32m[+] Password hash stored successfully!\033[0m");
+}
+
+bool verifyPassword(String password) {
+  if (!passwordHashExists()) {
+    return false;
+  }
+
+  uint8_t storedHash[32];
+  uint8_t storedSalt[16];
+  uint8_t computedHash[32];
+  EEPROM.begin(EEPROM_SIZE);
+  for (int i = 0; i < 32; i++) {
+    storedHash[i] = EEPROM.read(HASH_ADDRESS + i);
+  }
+  for (int i = 0; i < 16; i++) {
+    storedSalt[i] = EEPROM.read(SALT_ADDRESS + i);
+  }
+  EEPROM.end();
+  _generate_hash(password, computedHash, storedSalt);
+  bool match = true;
+  for (int i = 0; i < 32; i++) {
+    if (storedHash[i] != computedHash[i]) {
+      match = false;
+    }
+  }
+
+  return match;
+}
