1905bad1ef6ab043bd143bf29592faf487773279
mintRaven <debjeetbanerjee48@gmail.com>
2025-06-07 11:43:51 +0530

    add runtime state variables and frame controller variables

--- include/beacon.h
+++ include/beacon.h
@@ -19,6 +19,20 @@ int wifi_send_pkt_freedom(uint8 *buf, int len, bool sys_seq);
 #define RATE_CHECK_INTERVAL 1000
 #define TRANSMISSION_REPEATS 3
 
+extern uint8_t wifi_channels[];
+extern bool enable_wpa2_mode;
+extern bool pad_ssid_names;
+
+extern char blank_ssid[MAX_SSID_LENGTH];
+extern uint8_t current_channel_idx;
+extern uint8_t device_mac[MAC_ADDRESS_LENGTH];
+extern uint8_t active_channel;
+extern uint32_t time_now;
+extern uint32_t frame_size;
+extern uint32_t sent_packets;
+extern uint32_t last_attack;
+extern uint32_t rate_check_time;
+
 void switch_channel();
 void generate_random_mac();
 void initialize_system();
