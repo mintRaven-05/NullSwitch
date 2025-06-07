d18c94a82a85c1d0566c28e87aa44d3ba3806636
mintRaven <debjeetbanerjee48@gmail.com>
2025-06-07 11:28:59 +0530

    utility header block for shell

---
+++ include/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <FS.h>
+
+bool checkForAbort();
+void refreshLine();
+void showHelp();
+void displayMemInfo();
+void displayBanner();
+void displayBootInfo();
+
+#endif
