c84d2a15ed05ac8bd090831ed7612a5313c671e5
mintRaven <debjeetbanerjee48@gmail.com>
2025-06-07 11:35:46 +0530

    remove unwanted comments

--- include/shell.h
+++ include/shell.h
@@ -3,11 +3,9 @@
 
 #include <Arduino.h>
 
-// Shell constants
 #define MAX_COMMAND_LENGTH 128
 #define HISTORY_SIZE 10
 
-// Function declarations
 void addToHistory(String command);
 void showPrompt();
 void clearScreen();
