c9b70516fefc5e7dbc7d2c21d2d9823e7929519c
mintRaven <debjeetbanerjee48@gmail.com>
2025-06-07 11:37:20 +0530

    network scan command parse structs for record capturirng

--- include/netscan.h
+++ include/netscan.h
@@ -6,6 +6,67 @@
 #include <user_interface.h>
 #include "config.h"
 
+struct NetworkInfo {
+    String ssid;
+    String bssid;
+    uint8_t bssidBytes[6];
+    int32_t rssi;
+    String security;
+    int channel;
+  };
+  
+  struct ClientInfo {
+    uint8_t mac[6];
+    int8_t rssi;
+    uint32_t lastSeen;
+    uint32_t packetCount;
+    bool active;
+    String networkSSID;
+    String deviceType;
+    String macOUI;
+  };
+  
+  typedef struct {
+    signed rssi : 8;
+    unsigned rate : 4;
+    unsigned is_group : 1;
+    unsigned : 1;
+    unsigned sig_mode : 2;
+    unsigned legacy_length : 12;
+    unsigned damatch0 : 1;
+    unsigned damatch1 : 1;
+    unsigned bssidmatch0 : 1;
+    unsigned bssidmatch1 : 1;
+    unsigned MCS : 7;
+    unsigned CWB : 1;
+    unsigned HT_length : 16;
+    unsigned Smoothing : 1;
+    unsigned Not_Sounding : 1;
+    unsigned : 1;
+    unsigned Aggregation : 1;
+    unsigned STBC : 2;
+    unsigned FEC_CODING : 1;
+    unsigned SGI : 1;
+    unsigned rxend_state : 8;
+    unsigned ampdu_cnt : 8;
+    unsigned channel : 4;
+    unsigned : 12;
+  } wifi_pkt_rx_ctrl_t;
+  
+  typedef struct {
+    wifi_pkt_rx_ctrl_t rx_ctrl;
+    uint8_t buf[0];
+  } wifi_pkt_t;
+  
+  struct ieee80211_hdr {
+    uint16_t frame_ctrl;
+    uint16_t duration_id;
+    uint8_t addr1[6];
+    uint8_t addr2[6];
+    uint8_t addr3[6];
+    uint16_t seq_ctrl;
+  } __attribute__((packed));
+
 #define WIFI_PKT_MGMT 0x00
 #define WIFI_PKT_CTRL 0x01
 #define WIFI_PKT_DATA 0x02
